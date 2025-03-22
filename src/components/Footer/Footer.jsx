0edfd30b54da11a692b14351ffb8a7667462264d
Sajjad4297 <sajjad.ranjbaran.4297@gmail.com>
2025-03-22 16:06:38 +0330

    new start

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+export default function Footer() {
+  return (
+    <footer>
+        <div className='top-footer'></div>
+        <div className='bottom-footer'>
+            <h1></h1>
+        </div>
+    </footer>
+  )
+}
---
+++ src/components/Tag/Tag.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './tag.css'
+export default function Tag(value) {
+  return (
+    <div className='tag'></div>
+  )
+}
