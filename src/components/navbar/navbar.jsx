16941f8e787f6bc8e794afbcdaeb5a7430f4bd34
sjad002 <sajjadizady77@gmail.coim>
2025-03-24 11:05:30 +0000

    dcfvdfv

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,38 @@
+'use server'
 import React from 'react'
 import './navbar.css'
 import Image from 'next/image'
 import logo from '@/img/logo.png'
 import Link from 'next/link'
+import axios from 'axios'
 
-export default function Navbar() {
+export async function getServerSideProps() {
+    try {
+        const res = await axios.get('http://localhost:3000/api/topic'); // Fetch data using Axios
+        const data = res.data; // Axios automatically parses JSON
+        console.log(res)
+        return {
+            props: { data }, // Pass data as props
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+            props: { data: null }, // Handle errors gracefully
+        };
+    }
+}
+export default async function Navbar({ data }) {
     const link = [{
         text: 'درباره ما',
         href: '/'
     }, {
         text: 'تماس با ما',
-        href: '/admin'
-    }]
+        href: '/'
+    }];
+
+    console.log(data);
+
+
     return (
         <header>
             <div className='container'>
@@ -20,18 +41,21 @@ export default function Navbar() {
                 </div>
                 <div>
                     <ul className='ul1'>
-                            {
-                                link.map(link => (
-                                    <li key={link.text} className='li1'>
-                                        <Link href={link.href}  className='text-color'>{link.text}</Link>
-                                    </li>
+                        {
+                            link.map(link => (
+                                <li key={link.text} className='li1'>
+                                    <Link href={link.href} className='text-color'>{link.text}</Link>
+                                </li>
 
-                                ))}
-                            <form action="">
-                                <input type="search" placeholder='جستجو...' className='search-box' />
-                            </form>
+                            ))}
+                        <form action="">
+                            <input type="search" placeholder='جستجو...' className='search-box' />
+                        </form>
                     </ul>
                 </div>
+            </div>
+            <div className='dropdown'>
+
             </div>
         </header>
     )
