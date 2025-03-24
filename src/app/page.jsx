937eeaf961d73921d060eb88bc9af8ff04c90f54
sjad002 <sajjadizady77@gmail.coim>
2025-03-24 13:56:48 +0000

    man khodam (sajjad)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,18 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
   );
 }
--- src/components/navbar/NavItem.jsx
+++ src/components/navbar/NavItem.jsx
@@ -1,26 +1,20 @@
-'use client'
-import React from 'react'
-import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from "@nextui-org/react";
 import Link from 'next/link'
-export default function NavItem({ topic }) {
-    console.log(topic)
-    return (
 
-        <div>
-            <Dropdown>
-                <DropdownTrigger>
-                    <button className='text-color' style={{ cursor: 'pointer', background: 'none', border: 'none' }}>
-                        {topic.title}
-                    </button>
-                </DropdownTrigger>
-                <DropdownMenu aria-label="دسته‌بندی‌ها"  className="dropdown-menu">
-                    {topic.sub_topic.map((sub_topic, i) => (
-                        <DropdownItem key={sub_topic.name}>
-                            <Link href="/social" className='text-color'>{sub_topic.title}</Link>
-                        </DropdownItem>
-                    ))}
-                </DropdownMenu>
-            </Dropdown>
+
+export default function NavItem({ topic }) {
+
+
+    return (
+        <div className='nav-item'>
+            {topic.title}
+            <div className="drop-down">
+                {topic.sub_topic.map((sub_topic, i) => (
+                    <p>
+                        <Link href={sub_topic.name} className='text-color'>{sub_topic.title}</Link>
+                    </p>
+
+                ))}
+            </div>
         </div>
     )
 }
