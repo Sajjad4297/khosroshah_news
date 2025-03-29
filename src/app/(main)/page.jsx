a594d794ff71278c952f70eb5f4bcc6da630d02b
sjad002 <sajjadizady77@gmail.coim>
2025-03-29 12:23:59 +0000

    kir

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,9 +1,78 @@
-export default function HomePage() {
-    return (
-      <div className="home-content">
-        <h1>Welcome to the Main Site</h1>
-        <p>This content should appear between header and footer</p>
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+import Image from "next/image";
+import Link from "next/link";
+import "./Home.css";
+import img from '@/img/shah.webp';
+
+export default function Home() {
+  return (
+    <div className="Home-page">
+      <div className="right-page">
+        <div className='main'>
+          <div className='right-main'>
+            <div className='img-side'>
+              <Link href="/news/1">
+                <Image src={img} width={400} height={250} alt="تصویر خبر" />
+              </Link>
+            </div>
+            <div className='text-side'>
+              <p className="p">رضا شاه :</p>
+              <h3 className="title">
+                <Link href="/news/2" className="children-title">
+                  راهپیمایی امسال یکی از باعزت‌ترین راهپیمایی‌های روز قدس خواهد بود
+                </Link>
+              </h3>
+              <p className="discriptions">
+                رهبر معظم انقلاب اسلامی فرمودند: إن‌شاءالله راهپیمایی امسال، یکی از بهترین، پرشکوه‌ترین و باعزت‌ترین راهپیمایی‌های روز قدس خواهد بود.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="main-2">
+          <div className="top-main">
+            {[...Array(6)].map((_, index) => (
+              <Link href={`/news/${index + 3}`} key={index}>
+                <Image src={img} width={250} height={150} alt="تصویر خبر" />
+                <h3 className="title">
+                  <p className="children-title">رهبر انصارالله: ایران نقش محوری در حمایت از فلسطین دارد</p>
+                </h3>
+              </Link>
+            ))}
+          </div>
+          
+          <div className="Most-viewed-news">
+            <div className="Most-viewed-news-title">
+              <h3>پربازدیدترین اخبار</h3>
+            </div>
+            {[...Array(6)].map((_, index) => (
+              <div className="children-Most-viewed-news-top" key={index}>
+                <Link href={`/news/${index + 6}`}>جدیدترین لفاظی «ترامپ» درباره ایران</Link>
+                <p>دیروز ۲۱:۱۱</p>
+              </div>
+            ))}
+          </div>
+
+          <div className="Other-important-news">
+            <Link href="/important-news" className="Other-important-news-title">سایر اخبار مهم</Link>
+          </div>
+
+          <div className="other-news">
+            {[...Array(6)].map((_, index) => (
+              <div className="news" key={index}>
+                <Image src={img} width={200} height={120} alt="تصویر خبر" />
+                <div className="news-text">
+                  <h3>
+                    <Link href={`/news/${index + 12}`}>عراقچی: آرمان فلسطین فراموش شدنی نیست</Link>
+                  </h3>
+                  <p>
+                    امام خمینی در ۱۳ رمضان ۱۳۵۸ آخرین جمعه ماه رمضان را به نام روز قدس نامیدند. همان زمان چهره‌هایی چون سیمین دانشور، احمد شاملو و داریوش مهرجویی در بیانیه‌ای از راهپیمایی روز قدس حمایت کردند.
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,10 +5,10 @@ import logo from '@/img/logo.png'
 import Link from 'next/link'
 import axios from 'axios'
 import NavItem from './NavItem'
-async function getTopics() {
-    const res = await axios('http://localhost:3000/api/topic');
-    return res.data.topics;
-  }
+// async function getTopics() {
+//     const res = await axios('http://localhost:3000/api/topics');
+//     return res.data.topics;
+//   }
 export default async function Navbar() {
     const link = [{
         text: 'درباره ما',
@@ -17,8 +17,287 @@ export default async function Navbar() {
         text: 'تماس با ما',
         href: '/'
     }];
-    const topics = await getTopics();
-
+    
+    const topics = [
+        {
+          "id": 1,
+          "title": "سیاسی",
+          "name": "politic",
+          "sub_topic": [
+            {
+              "id": 1,
+              "title": "امام و رهبری",
+              "name": "leaders",
+              "topicId": 1
+            },
+            {
+              "id": 2,
+              "title": "دولت",
+              "name": "government",
+              "topicId": 1
+            },
+            {
+              "id": 3,
+              "title": "مجلس",
+              "name": "parliament",
+              "topicId": 1
+            },
+            {
+              "id": 4,
+              "title": "امنیتی و دفاعی",
+              "name": "security-defensive",
+              "topicId": 1
+            },
+            {
+              "id": 5,
+              "title": "سیاست خارجی",
+              "name": "foreignpolitic",
+              "topicId": 1
+            },
+            {
+              "id": 6,
+              "title": "انرژی هسته‌‎ای",
+              "name": "nuclear_energy",
+              "topicId": 1
+            },
+            {
+              "id": 7,
+              "title": "احزاب و تشکل‌ها ",
+              "name": "parties",
+              "topicId": 1
+            }
+          ]
+        },
+        {
+          "id": 2,
+          "title": "اجتماعی",
+          "name": "society",
+          "sub_topic": [
+            {
+              "id": 8,
+              "title": "انتظامی و حوادث",
+              "name": "security-accidents",
+              "topicId": 2
+            },
+            {
+              "id": 9,
+              "title": "شهری",
+              "name": "urban",
+              "topicId": 2
+            },
+            {
+              "id": 10,
+              "title": "آموزش و پرورش",
+              "name": "education",
+              "topicId": 2
+            },
+            {
+              "id": 11,
+              "title": "حقوقی و قضایی",
+              "name": "justice",
+              "topicId": 2
+            },
+            {
+              "id": 12,
+              "title": "رفاه و آسیب‌های اجتماعی",
+              "name": "welfare-socialdamage",
+              "topicId": 2
+            },
+            {
+              "id": 13,
+              "title": "خانواده",
+              "name": "family",
+              "topicId": 2
+            },
+            {
+              "id": 14,
+              "title": "دانشگاه و حوزه",
+              "name": "university-howzeh",
+              "topicId": 2
+            },
+            {
+              "id": 15,
+              "title": "سلامت",
+              "name": "health",
+              "topicId": 2
+            },
+            {
+              "id": 16,
+              "title": "محیط زیست",
+              "name": "environment",
+              "topicId": 2
+            }
+          ]
+        },
+        {
+          "id": 3,
+          "title": "اقتصادی",
+          "name": "economy",
+          "sub_topic": [
+            {
+              "id": 17,
+              "title": "اقتصاد ایران",
+              "name": "iran-economics",
+              "topicId": 3
+            },
+            {
+              "id": 18,
+              "title": "اقتصاد جهان",
+              "name": "world-economics",
+              "topicId": 3
+            },
+            {
+              "id": 19,
+              "title": "انرژی",
+              "name": "energy",
+              "topicId": 3
+            },
+            {
+              "id": 20,
+              "title": "بانک، بیمه و بورس",
+              "name": "banking-insurance",
+              "topicId": 3
+            },
+            {
+              "id": 21,
+              "title": "راه و مسکن",
+              "name": "civil",
+              "topicId": 3
+            },
+            {
+              "id": 22,
+              "title": "صنعت، معدن و تجارت",
+              "name": "mining-trade",
+              "topicId": 3
+            },
+            {
+              "id": 23,
+              "title": "کار و تعاون",
+              "name": "occupation",
+              "topicId": 3
+            },
+            {
+              "id": 24,
+              "title": "کشاورزی و دامپروری",
+              "name": "agriculture",
+              "topicId": 3
+            }
+          ]
+        },
+        {
+          "id": 4,
+          "title": "فرهنگی",
+          "name": "culture",
+          "sub_topic": [
+            {
+              "id": 25,
+              "title": "دین و اندیشه",
+              "name": "religion-thought",
+              "topicId": 4
+            },
+            {
+              "id": 26,
+              "title": "رادیو و تلویزیون",
+              "name": "radio-tv",
+              "topicId": 4
+            },
+            {
+              "id": 27,
+              "title": "رسانه",
+              "name": "media",
+              "topicId": 4
+            },
+            {
+              "id": 28,
+              "title": "سینما و تئاتر",
+              "name": "cinema-drama",
+              "topicId": 4
+            },
+            {
+              "id": 29,
+              "title": "فرهنگ عمومی",
+              "name": "public-culture",
+              "topicId": 4
+            },
+            {
+              "id": 30,
+              "title": "کتاب، شعر و ادب",
+              "name": "book-poem-lyrics",
+              "topicId": 4
+            },
+            {
+              "id": 31,
+              "title": "میراث فرهنگی و گردشگری",
+              "name": "cultural-heritage",
+              "topicId": 4
+            }
+          ]
+        },
+        {
+          "id": 5,
+          "title": "ورزشی",
+          "name": "sport",
+          "sub_topic": [
+            {
+              "id": 32,
+              "title": "فوتبال",
+              "name": "football",
+              "topicId": 5
+            },
+            {
+              "id": 33,
+              "title": "توپ و تور",
+              "name": "ball-tour",
+              "topicId": 5
+            },
+            {
+              "id": 34,
+              "title": "کشتی",
+              "name": "wrestling",
+              "topicId": 5
+            },
+            {
+              "id": 35,
+              "title": "ورزش‌های رزمی",
+              "name": "martialarts",
+              "topicId": 5
+            },
+            {
+              "id": 36,
+              "title": "سایر ورزش‌ها",
+              "name": "othersport",
+              "topicId": 5
+            },
+            {
+              "id": 37,
+              "title": "یادداشت ورزشی",
+              "name": "sport-note",
+              "topicId": 5
+            }
+          ]
+        },
+        {
+          "id": 6,
+          "title": "دانش و فناوری",
+          "name": "tech",
+          "sub_topic": [
+            {
+              "id": 38,
+              "title": "ارتباطات و فناوری اطلاعات",
+              "name": "ict",
+              "topicId": 6
+            },
+            {
+              "id": 39,
+              "title": "فناوری‌های نوین ",
+              "name": "newtechnologies",
+              "topicId": 6
+            }
+          ]
+        }
+      ];
+        console.log(topics);
+        
 
     return (
         <header>
@@ -27,22 +306,24 @@ export default async function Navbar() {
                     <Image src={logo} width={350} height={120} className='logo' alt='logo' />
                 </div>
                 <div>
-                    <ul className='ul1'>
-                        {
-                            link.map(link => (
-                                <li key={link.text} className='li1'>
-                                    <Link href={link.href} className='text-color'>{link.text}</Link>
-                                </li>
+                        <div className='div1'>
+                            {
+                                link.map(link => (
+                                    <li key={link.text} className='li1'>
+                                        <Link href={link.href} className='text-color'>{link.text}</Link>
+                                    </li>
 
-                            ))}
+                                ))}
+                        </div>
+                        <div>
                         <form action="">
-                            <input type="search" placeholder='جستجو...' className='search-box' />
-                        </form>
-                    </ul>
+                        <input type="search" placeholder='جستجو...' className='search-box' />
+                    </form>
+                        </div>
                 </div>
             </div>
             <div className='dropdown'>
-                {topics.map((topic,i) => <NavItem topic={topic} key={i} />)}
+                {topics.map((topic, i) => <NavItem topic={topic} key={i} />)}
 
             </div>
         </header>
