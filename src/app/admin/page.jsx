4d0197df88dfa77a7a58d19e7d2e243d459aa080
Sajjad4297 <sajjad.ranjbaran.4297@gmail.com>
2025-05-27 21:55:27 +0330

    admin delete news

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -5,17 +5,17 @@ import './news.css'
 export default function page() {
     const [news , setNews] = useState()
         useEffect(() => {
-            fetch("https://backend.navayetabriz.ir/api/news-over-view", {
+            fetch("https://backend.navayetabriz.ir/api/news/all", {
                 method: "GET",
             })
             .then((res) => res.json())
-            .then(res => {setNews(res.data);})
+            .then(res => {setNews(res.data.sort((a , b)=> b.id - a.id));})
             .catch(err => console.error('Error fetching tags:', err));
         }, []);
 
   return (
     <div className='admin-news-container'>
-        {news?.map((item,i) => <AdminNews key={i} title={item.title} description={item.description} image={"https://backend.navayetabriz.ir/uploads/"+item.image} />)}
+        {news?.map((item,i) => <AdminNews key={i} setNews={setNews} id={item.id} title={item.title} description={item.description} image={"https://backend.navayetabriz.ir/uploads/"+item.image} />)}
     </div>
   )
 }
--- src/components/Admin/AdminNews.jsx
+++ src/components/Admin/AdminNews.jsx
@@ -2,17 +2,39 @@
 import React from 'react';
 import { Card } from 'antd';
 import Image from 'next/image';
-
+import deleteIcon from '@/img/delete.svg'
 const { Meta } = Card;
 
-const AdminNews = ({ title, description, image }) => (
-    <Card
-        hoverable
-        style={{ width: 320 }}
-        cover={<Image alt={title} src={image} width= {320} height={180} />}
-    >
-        <Meta title={title} description={description} />
-    </Card>
-);
+const AdminNews = ({ title, description, image,id,setNews }) =>{
 
+    const handleDelete = () => {
+        if (!window.confirm("برای حذف این خبر مطمئن هستید؟")) {
+            return;
+        }
+        fetch(`https://backend.navayetabriz.ir/api/news/${id}`, {
+            method: "DELETE",
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            setNews((prevNews) => prevNews.filter((item) => item.id != id));
+        })
+        .catch((error) => {
+            console.error('Error deleting news:', error);
+        });
+    };
+
+    return(
+    <div style={{position: 'relative'}}>
+        <Card
+            hoverable
+            style={{ width: 320}}
+            cover={<Image alt={title} src={image} width={320} height={180} />}
+        >
+            <Meta title={title} description={description} />
+        </Card>
+        <Image id='delete-icon' alt={'delete'} src={deleteIcon} width={40} height={40} onClick={handleDelete} />
+
+    </div>
+);
+}
 export default AdminNews;
